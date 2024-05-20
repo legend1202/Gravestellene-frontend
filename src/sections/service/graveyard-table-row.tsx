f240bfc18bc3f1234e5771eb7e46b0db8f25bb98
DESKTOP-MDCHV7B\wa <phoenixdev0211@gmail.com>
2024-05-19 20:52:02 -0700

    feat:services list

--- src/sections/service/graveyard-table-row.tsx
+++ src/sections/service/graveyard-table-row.tsx
@@ -1,15 +1,15 @@
-import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
-import Stack from "@mui/material/Stack";
-import Avatar from "@mui/material/Avatar";
-import { GridCellParams } from "@mui/x-data-grid";
-import ListItemText from "@mui/material/ListItemText";
-import LinearProgress from "@mui/material/LinearProgress";
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import Avatar from '@mui/material/Avatar';
+import { GridCellParams } from '@mui/x-data-grid';
+import ListItemText from '@mui/material/ListItemText';
+import LinearProgress from '@mui/material/LinearProgress';
 
-import { fCurrency } from "src/utils/format-number";
+import { fCurrency } from 'src/utils/format-number';
 // import { fTime, fDate } from "src/utils/format-time";
 
-import Label from "src/components/label";
+import Label from 'src/components/label';
 
 // ----------------------------------------------------------------------
 
@@ -23,21 +23,21 @@ export function RenderCellPrice({ params }: ParamsProps) {
 
 export function RenderCellApprove({ params }: ParamsProps) {
   return (
-    <Label variant="soft" color={(params.row.approved && "info") || "default"}>
-      {params.row.approved ? "Approved" : ""}
+    <Label variant="soft" color={(params.row.approved && 'info') || 'default'}>
+      {params.row.approved ? 'Approved' : ''}
     </Label>
   );
 }
 
-export function RenderCellLocation({ params }: ParamsProps) {
+export function RenderCellDescription({ params }: ParamsProps) {
   return (
     <ListItemText
-      primary={params.row.location}
-      primaryTypographyProps={{ typography: "body2", noWrap: true }}
+      primary={params.row.description}
+      primaryTypographyProps={{ typography: 'body2', noWrap: true }}
       secondaryTypographyProps={{
         mt: 0.5,
-        component: "span",
-        typography: "caption",
+        component: 'span',
+        typography: 'caption',
       }}
     />
   );
@@ -45,19 +45,18 @@ export function RenderCellLocation({ params }: ParamsProps) {
 
 export function RenderCellStock({ params }: ParamsProps) {
   return (
-    <Stack sx={{ typography: "caption", color: "text.secondary" }}>
+    <Stack sx={{ typography: 'caption', color: 'text.secondary' }}>
       <LinearProgress
         value={(params.row.available * 100) / params.row.quantity}
         variant="determinate"
         color={
-          (params.row.inventoryType === "out of stock" && "error") ||
-          (params.row.inventoryType === "low stock" && "warning") ||
-          "success"
+          (params.row.inventoryType === 'out of stock' && 'error') ||
+          (params.row.inventoryType === 'low stock' && 'warning') ||
+          'success'
         }
         sx={{ mb: 1, height: 6, maxWidth: 80 }}
       />
-      {!!params.row.available && params.row.available}{" "}
-      {params.row.inventoryType}
+      {!!params.row.available && params.row.available} {params.row.inventoryType}
     </Stack>
   );
 }
@@ -65,13 +64,6 @@ export function RenderCellStock({ params }: ParamsProps) {
 export function RenderCellGraveyard({ params }: ParamsProps) {
   return (
     <Stack direction="row" alignItems="center" sx={{ py: 2, width: 1 }}>
-      <Avatar
-        alt={params.row.name}
-        src={params.row.coverUrl}
-        variant="rounded"
-        sx={{ width: 64, height: 64, mr: 2 }}
-      />
-
       <ListItemText
         disableTypography
         primary={
@@ -81,20 +73,17 @@ export function RenderCellGraveyard({ params }: ParamsProps) {
             variant="subtitle2"
             onClick={params.row.onViewRow}
             href="/graveyard/i"
-            sx={{ cursor: "pointer" }}
+            sx={{ cursor: 'pointer' }}
           >
             {params.row.name}
           </Link>
         }
         secondary={
-          <Box
-            component="div"
-            sx={{ typography: "body2", color: "text.disabled" }}
-          >
+          <Box component="div" sx={{ typography: 'body2', color: 'text.disabled' }}>
             {params.row.category}
           </Box>
         }
-        sx={{ display: "flex", flexDirection: "column" }}
+        sx={{ display: 'flex', flexDirection: 'column' }}
       />
     </Stack>
   );
